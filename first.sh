faf58624699629c2671400f4a59c004a961d714c
SK <test@gmail.com>
2023-09-28 23:26:04 +1000

    create and read file

--- first.sh
+++ first.sh
@@ -5,20 +5,24 @@ user='sskaran'
 echo "First shell script"
 
 # Create new file 
-echo Create new file 
+echo "  Create new file"
 echo "created by $user"  > new1.txt
 
 # List files
-echo List files
+echo "  List files"
 ls -ltr
 
 # edit file
-Echo Touch file
+echo "  Touch file"
 touch new1.txt
 
 # List files
-echo List files
+echo "  List files"
 ls -ltr
+
+# Read file
+cat new1.txt
+
 
 
 
