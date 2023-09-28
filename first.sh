645b7f8d131464f8ee7d7599b5688a12bccc7bda
sushantkarande18 <sushantkarande18@gmail.com>
2023-09-28 23:21:31 +1000

    Update first.sh

--- first.sh
+++ first.sh
@@ -5,15 +5,19 @@ user='sskaran'
 echo "First shell script"
 
 # Create new file 
-echo "created by ((user))"  >> new1.txt
+echo Create new file 
+echo "created by $user"  > new1.txt
 
 # List files
+echo List files
 ls -ltr
 
 # edit file
+Echo Touch file
 touch new1.txt
 
 # List files
+echo List files
 ls -ltr
 
 
