4cfe7e73f417715c80cc1cf847693c772233db42
SK <test@gmail.com>
2023-09-28 23:18:40 +1000

    first shell script

---
+++ first.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+user='sskaran'
+
+echo "First shell script"
+
+# Create new file 
+echo "created by ((user))"  >> new1.txt
+
+# List files
+ls -ltr
+
+# edit file
+touch new1.txt
+
+# List files
+ls -ltr
+
+
+
+
+
+
+
+
+
